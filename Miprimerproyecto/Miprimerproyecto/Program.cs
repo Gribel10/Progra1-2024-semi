1a3417d3e0e4fbe323feb1f240b33a1f181f0e7c
Kelly Gribel Ayala Torres <USSS023424@ugb.edu.sv>
2024-07-28 21:45:34 -0600

    suma de dos numeros decimales

--- Miprimerproyecto/Miprimerproyecto/Program.cs
+++ Miprimerproyecto/Miprimerproyecto/Program.cs
@@ -10,18 +10,16 @@ namespace Miprimerproyecto
     {
         static void Main(string[] args)
         {
-            //Suma de dos numeros
+            //Suma de dos numeros decimales
 
             Console.Write("num1:");
-            double num1 = double.Parse(Console.ReadLine());
+            double num1 = double.Parse(Console.ReadLine()); //"5" ->
 
             Console.Write("num2:");
             double num2 = double.Parse(Console.ReadLine());
 
             double respuesta = num1 + num2;
             Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
-
-
 
             //Pausa
             Console.ReadLine();
