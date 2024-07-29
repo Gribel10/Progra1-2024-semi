234f2ef76399d2d716bc367010de4c8ecae3dc28
Kelly Gribel Ayala Torres <USSS023424@ugb.edu.sv>
2024-07-28 21:23:30 -0600

    Suma de dos numeros

--- Miprimerproyecto/Miprimerproyecto/Program.cs
+++ Miprimerproyecto/Miprimerproyecto/Program.cs
@@ -10,7 +10,20 @@ namespace Miprimerproyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            //Suma de dos numeros
+
+            Console.Write("num1:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("num2:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+
+
+            //Pausa
             Console.ReadLine();
         }
     }
